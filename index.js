bc0ecf8350265427e873997c04e55de5635e2cc4
Ruben Diaz Mantilla <rubendm@MacBook-Pro-de-Ruben.local>
2020-07-14 17:57:32 +0200

    instalamos nodemon

--- index.js
+++ index.js
@@ -6,4 +6,12 @@ const PORT = 5000;
 
 app.use(bodyParser.json());
 
-app.listen(PORT, () => console.log(`Server running on port: http://localhost ${PORT}`));
+//Rutas de la aplicacion
+
+app.get('/', (req, res) =>{
+    console.log('[TEST]!');
+    res.send('Hello from Homepage');
+});
+
+
+app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));

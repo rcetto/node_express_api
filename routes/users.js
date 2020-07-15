f2ef34a69278c77a297334c9f2bc54108e935e92
Ruben Diaz Mantilla <rubendm@MacBook-Pro-de-Ruben.local>
2020-07-15 17:38:13 +0200

    Añadimos el metodo post para añadir usuarios

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,35 @@ import express from 'express';
 
 const router =  express.Router();
 
+
+const users = [
+    {
+        firstName: "John",
+        lastName: "Doe",
+        age: 25
+    },
+    {
+        firstName: "Jane",
+        lastName: "Doe",
+        age: 24
+    }
+
+];
+
+
 // All routes in here are starting with /users
 router.get('/', (req, res) => {
-    res.send('Hello');
+    console.log(users);
+    res.send(users);
 });
+
+// Router para introducir info
+router.post('/', (req, res) => {
+    //user.push
+    const user = req.body
+    users.push(user);
+    res.send(`User with the name ${user.firstName} added to the database!`);
+});
+
 
 export default router;

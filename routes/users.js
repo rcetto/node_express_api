7705f8482149f4384d59e02c64278c8019033da9
Ruben Diaz Mantilla <rubendm@MacBook-Pro-de-Ruben.local>
2020-07-16 09:00:43 +0200

    final del video

--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,17 @@
 import express from 'express';
 
-import { v4 as uuidv4 } from 'uuid';
+import { getUsers, createUser, getUser, deleteUser, updateUser } from '../controllers/users.js';
 
+const router = express.Router();
 
-const router =  express.Router();
+router.get('/', getUsers);
 
+router.post('/', createUser);
 
-let users = [];
+router.get('/:id', getUser);
 
+router.delete('/:id', deleteUser);
 
-// All routes in here are starting with /users
-router.get('/', (req, res) => {
-    console.log(users);
-    res.send(users);
-});
-
-// Router para introducir info
-router.post('/', (req, res) => {
-    //user.push
-    const user = req.body
-    
-    users.push({ ...user, id: uuidv4() });
-    res.send(`User with the name ${user.firstName} added to the database!`);
-});
-
+router.patch('/:id', updateUser);
 
 export default router;
